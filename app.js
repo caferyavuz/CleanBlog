7d2398c1e7e3ad58d4c7377b1ca13aad1bed55e3
Recep Cafer Yavuz <caferyavuz134@gmail.com>
2022-04-10 21:03:04 +0300

    mongoose added

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
 
+const Photo = require('./models/Photo')
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
+// MIDDLEWARE
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
+app.use(express.json())
 
 
 
-app.get('/',(req,res)=>{
-    res.render("index")
+app.get('/',async(req,res)=>{
+    const photos = await Photo.find()
+    res.render("index",{
+        photos:photos
+    })
 })
 
-app.get('/index',(req,res)=>{
-    res.render("index")
+app.get('/index',async(req,res)=>{
+    const photos = await Photo.find()
+    res.render("index",{
+        photos:photos
+    })
 })
 
 app.get('/about',(req,res)=>{
@@ -27,11 +41,17 @@ app.get('/add_post',(req,res)=>{
 })
 
 app.get('/post',(req,res)=>{
+    
     res.render("post")
 })
 
 app.get('*',(req,res)=>{
     res.send('<h2> 404 Not Found</h2>')
+})
+
+app.post('/addPost',async(req,res)=>{
+    await Photo.create(req.body)
+    res.redirect("/index")
 })
 
 app.listen(port,()=>{
